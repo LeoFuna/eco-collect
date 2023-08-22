fc4b9854f8e88fb01d5ab39f661700712608c694
leofuna <leo.funa@gmail.com>
2023-08-22 18:46:31 -0300

    feat: migrations

---
+++ src/database/migrations/02_create_create_collect_point_residue.ts
@@ -0,0 +1,20 @@
+import { Knex } from "knex";
+
+export async function up(knex: Knex) {
+  return knex.schema.createTable('collect_point_residues', table => {
+    table.increments('id').primary();
+    table.integer('collect_point_id')
+      .notNullable()
+      .references('id')
+      .inTable('collect_points');
+    table.integer('residue_id')
+      .notNullable()
+      .references('id')
+      .inTable('residues');
+  })
+
+}
+
+export async function down(knex: Knex) {
+  return knex.schema.dropTable('collect_point_residues');
+}

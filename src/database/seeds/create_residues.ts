cc58ebae8af77bd6e11ca8788fc3f2126581edb4
leofuna <leo.funa@gmail.com>
2023-08-23 15:23:23 -0300

    fix: seed on create residues svg path adjust

--- src/database/seeds/create_residues.ts
+++ src/database/seeds/create_residues.ts
@@ -2,11 +2,11 @@ import { Knex } from "knex";
 
 export async function seed(knex: Knex) {
   await knex('residues').insert([
-    { name: 'Lâmpadas', image: 'lampadas.svg' },
-    { name: 'Pilhas e Baterias', image: 'baterias.svg' },
-    { name: 'Papéis e Papelão', image: 'papeis-papelao.svg' },
-    { name: 'Resíduos Eletrônicos', image: 'eletronicos.svg' },
-    { name: 'Resíduos Orgânicos', image: 'organicos.svg' },
-    { name: 'Óleo de Cozinha', image: 'oleo.svg' },
+    { name: 'Lâmpadas', image: 'light-bulb.svg' },
+    { name: 'Pilhas e Baterias', image: 'battery.svg' },
+    { name: 'Papéis e Papelão', image: 'paper.svg' },
+    { name: 'Resíduos Eletrônicos', image: 'electronic.svg' },
+    { name: 'Resíduos Orgânicos', image: 'organic.svg' },
+    { name: 'Óleo de Cozinha', image: 'oil.svg' },
   ])
 }

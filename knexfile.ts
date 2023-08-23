819fdb7fade030a6aa5824ba2c3b223befc13c81
leofuna <leo.funa@gmail.com>
2023-08-23 18:14:19 -0300

    build: active foreing keys constrains for sqlite

--- knexfile.ts
+++ knexfile.ts
@@ -3,7 +3,12 @@ import path from 'path'
 module.exports = {
   client: 'sqlite3',
   connection: {
-    filename: path.join(process.cwd(), 'src', 'database', 'database.sqlite')
+    filename: path.join(process.cwd(), 'src', 'database', 'database.sqlite'),
+  },
+  pool: {
+    afterCreate: (conn: any, cb: any) => {
+      conn.run('PRAGMA foreign_keys = ON', cb);
+    },
   },
   migrations: {
     directory: path.resolve(__dirname, 'src', 'database', 'migrations')

70b0bb597d43be26919de3600ba4cdac0fc5f3f4
leofuna <leo.funa@gmail.com>
2023-08-23 15:23:49 -0300

    fix: some fixes on residues api route

--- src/app/api/residues/route.ts
+++ src/app/api/residues/route.ts
@@ -1,16 +1,16 @@
-import knexConnection from "./../../../database/connection";
+import knexConnection from "@/database/connection";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request: Request, response: Response) {
+export async function GET() {
   const residues = await knexConnection('residues').select('*');
 
   return NextResponse.json({ residues });
 }
 
-export async function POST(request: Request, response: Response) {
+export async function POST(request: NextRequest) {
   const body = await request.json();
 
-  if (!body) return NextResponse.json({ error: 'Invalid body' });
+  if (!Object.keys(body).length) return NextResponse.json({ error: 'Invalid body' }, { status: 400 });
   
   const residues = await knexConnection('residues').insert(body);
 

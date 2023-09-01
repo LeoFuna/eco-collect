64ceeb71f83acd50f7c16b6fe17b8d07af9ca018
leofuna <leo.funa@gmail.com>
2023-09-01 17:19:52 -0300

    style: header component removing flex

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default function Header() {
   return (
     <header>
-      <div className="flex mt-11 ml-40 fixed">
+      <div className="top-0 left-0 mt-11 ml-40 fixed">
         <div className="relative w-44 h-11">
           <Image src='/logo.svg' fill priority alt="Ecoleta logo" />
         </div>

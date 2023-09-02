e7b4bd83736889dec98cd7069348208c92802f77
leofuna <leo.funa@gmail.com>
2023-09-02 08:23:47 -0300

    style: home css with classes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,26 @@
 import Image from "next/image";
-import { FiLogIn } from "react-icons/fi";
+import './home.css';
+import ButtonWithIcon from "@/components/Home/ButtonWithIcon";
 
 export default function Home() {
   return (
     <main className="w-screen h-screen flex">
-      <div className="m-40 flex w-full justify-between items-center">
-        <div className="max-w-3xl">
-          <h1 className="text-5xl font-extrabold break-words leading-snug">Seu marketplace</h1>
-          <h1 className="text-5xl font-extrabold break-words leading-snug">de coleta de resíduos.</h1>
-          <p className="my-10 text-2xl leading-snug max-w-xl">Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</p>
-          <button className="rounded-lg w-4/6 text-white flex items-center bg-secondary hover:opacity-90">
-            <div className="p-6">
-              <FiLogIn size={22} />
-            </div>
-            <strong className="p-6 w-full bg-primary rounded-r-lg">Cadastre um ponto de coleta</strong>
-          </button>
+      <div className="main-content">
+        <div className="hero-informations">
+          <h1 className="hero-title">Seu marketplace</h1>
+          <h1 className="hero-title">de coleta de resíduos.</h1>
+          <p className="hero-subtitle">
+            Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.
+          </p>
+          <ButtonWithIcon />
         </div>
-        <div className="relative w-4/6 min-w-40 h-full mt-36">
-          <Image src='/home-background.svg' priority fill alt="teste" />
+        <div className="hero-banner">
+          <Image
+            src='/home-background.svg'
+            priority
+            fill
+            alt="home image background"
+          />
         </div>
       </div>
     </main>

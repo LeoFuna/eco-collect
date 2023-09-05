a8c8a6d1e732dafe8d477a8fca200b91349b6909
leofuna <leo.funa@gmail.com>
2023-09-05 11:47:00 -0300

    feat: collect point interface

---
+++ src/entities/CollectPoint.ts
@@ -0,0 +1,11 @@
+interface ICollectPoint {
+  id?: number;
+  image?: string;
+  name: string;
+  email: string;
+  whatsapp: string;
+  latitude: number;
+  longitude: number;
+  city: string;
+  uf: string;
+}
